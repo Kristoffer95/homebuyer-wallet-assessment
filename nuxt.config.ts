2949661dc99a246a426ea2a758592213ca0c8f8d
kristoffer95 <29176224+Kristoffer95@users.noreply.github.com>
2024-07-30 10:16:59 +0800

    chore: added .npmrc and, set nuxt to render as SPA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  ssr: false,
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+
   shadcn: {
     /**
      * Prefix for all the imported component
